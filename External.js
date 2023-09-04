39cd5adfba49f3180f65a75fdfa060f7637ae54f
Coding001Z <iammarshal1914@gmail.com>
2023-09-04 22:43:53 +0700

    first commit

---
+++ External.js
@@ -0,0 +1,15 @@
+//document.write คือ แสดงเป็นข้อความ ตัวแปร ตัวเลข หรือแท๊ก HTML ในเว็บ จะแสดงในหน้าเว็บ
+document.write("<h1>NattananC</h1>");
+document.write("<p>GAmeplay</p>");
+
+//alert คือ แสดงแจ้งเตือนผู้ใช้ในหน้าเว็บเป็นแบบ POP UP
+alert("Say hi");
+
+//console.log คือ คำสั่งสำหรับ debug ค่าต่างๆ แต่จะไม่แสดงผลในหน้าเว็บจะแสดงเป็น ข้อความ ตัวเลข หรือตัวแปร ก็ได้ โดยแสดงผลใน Tab console ใน Web browser
+console.log("Goodbye");
+
+//console.error คือ การแสดงในรูปแบบ Error ออกมาใน Console
+console.error("Virus!");
+
+//console.warn คือ การแจ้งเตือน ออกมาในส่วน Console
+console.warn("Warning Virus");
